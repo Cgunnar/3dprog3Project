b5b1696563078f014b2e384d65118f59174a565e
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-04-20 11:04:41 +0200

    set up pch

--- 3dprog3Project/Src/Source.cpp
+++ 3dprog3Project/Src/Source.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "pch.h"
 
+#include "Application.h"
 int main()
 {
+	std::cout << "Start" << std::endl;
+	Application* myApp = new Application();
+	delete myApp;
+	std::cout << "Exit" << std::endl;
 	return 0;
 }
---
+++ 3dprog3Project/Src/pch.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define NOMINMAX
+#include <Windows.h>
+
+#include <assert.h>
+#include <iosfwd>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+#include <filesystem>
+
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+#include <memory>
+
+#include <string>
+#include <vector>
+#include <array>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <variant>
+#include <optional>
+#include <functional>
+
+#include <mutex>
+#include <thread>
+#include <atomic>
+#include <semaphore>
+#include <future>
